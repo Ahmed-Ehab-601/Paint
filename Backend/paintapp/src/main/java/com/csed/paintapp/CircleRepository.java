4d8f3b73dd06b28af9fab3ee9623771157c63a75
SamaaIbrahim <es-samaa.ibrahim2027@alexu.edu.eg>
2024-11-25 15:45:06 +0200

    add packages

--- Backend/paintapp/src/main/java/com/csed/paintapp/CircleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.csed.paintapp;
-
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CircleRepository extends CrudRepository<Circle,Integer> {
-}
